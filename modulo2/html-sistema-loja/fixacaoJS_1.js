2c2e8b187796576f9b404e54a55f55f82441de97
Thiago Vieira <thiago.cng@gmail.com>
2021-12-14 17:07:26 -0300

    update exercicios HTML

--- modulo2/html-sistema-loja/fixacaoJS_1.js
+++ modulo2/html-sistema-loja/fixacaoJS_1.js
@@ -1,6 +1,7 @@
 function calculaSalario(qtdeCarrosVendidos, valorTotalVendas)
 {
-    
-   
+    let salario = 2000 + (100 * qtdeCarrosVendidos) + (valorTotalVendas * 0.05);
+
+    return salario;
    
 }
