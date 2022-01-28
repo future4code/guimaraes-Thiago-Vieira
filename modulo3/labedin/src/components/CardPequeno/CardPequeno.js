c25fe39d8923f467055060f2a48b8c2284c8cd19
Thiago <thiago.cng@gmail.com>
2022-01-27 23:03:55 -0300

    entrega atividade labedin

---
+++ modulo3/labedin/src/components/CardPequeno/CardPequeno.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './CardPequeno.css'
+
+function CardPequeno(props) {
+    return (
+        <div className="smallcard-container">
+            <img src={ props.imagem } />
+            <div>
+                <h4>{ props.field }:&nbsp;</h4>
+                <p>{ props.value }</p>
+            </div>
+        </div>
+    )
+}
+
+export default CardPequeno;
